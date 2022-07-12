5ef6a5e420592ea41fa5609e1457b52c94a17e86
sudug <60267838+expfukck@users.noreply.github.com>
2022-07-12 15:47:21 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 docker build -t x-ui .
-docker run --name x-ui -d --net=host --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/Internet/x-ui:/etc/x-ui -v ~/Internet/x-ray:/usr/local/x-ui/bin x-ui
+docker run --name x-ui -d --net=host --restart always --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/Internet/x-ui:/etc/x-ui -v ~/Internet/x-ray:/usr/local/x-ui/bin x-ui
