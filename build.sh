bffda8d1e613b860de53026cd731d9083b595a48
sudug <60267838+expfukck@users.noreply.github.com>
2022-07-12 15:21:23 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,2 @@
-apt-get install -y unzip wget
 docker build -t x-ui .
-wget https://github.com/XTLS/Xray-core/releases/download/v1.5.5/Xray-linux-64.zip
-unzip Xray-linux-64.zip -d x-ray
-mv x-ray/xray x-ray/xray-linux-amd64
-rm Xray-linux-64.zip
-docker run --name x-ui -d -p 54321:54321 --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/Internet/x-ui:/etc/x-ui -v ~/Internet/x-ray:/usr/local/x-ui/bin x-ui
+docker run --name x-ui -d --net=host --tmpfs /tmp --tmpfs /run --tmpfs /run/lock -v /sys/fs/cgroup:/sys/fs/cgroup:ro -v ~/Internet/x-ui:/etc/x-ui -v ~/Internet/x-ray:/usr/local/x-ui/bin x-ui
